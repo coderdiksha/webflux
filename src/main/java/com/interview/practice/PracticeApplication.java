31f33c1047a85639e2cc9f83c7917ac0d41cb66f
diksha <emails2diksha@gmail.com>
2024-02-05 21:54:54 +0530

    feat: add a basic working webservice in webflux

---
+++ src/main/java/com/interview/practice/PracticeApplication.java
@@ -0,0 +1,17 @@
+package com.interview.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class PracticeApplication {
+
+	public static void main(String[] args) {
+
+		ConfigurableApplicationContext context = SpringApplication.run(PracticeApplication.class, args);
+		GreetingClient greetingClient = context.getBean(GreetingClient.class);
+		// We need to block for the content here or the JVM might exit before the message is logged
+		System.out.println(">> message = " + greetingClient.getMessage().block());
+	}
+}
